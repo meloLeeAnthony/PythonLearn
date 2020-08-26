579b8db16461be374ebb4f38395cfd33de0ecf05
li.chunlong <lcl253137@alibaba-inc.com>
2020-08-26 18:01:01 +0800

    code format

---
+++ basic/TypeTest.py
@@ -0,0 +1,8 @@
+# coding=UTF-8
+"""
+Python是动态类型语言：变量不需要显示声明类型。根据变量引用的对象，Python解释器自动确定数据类型
+"""
+a = 10
+print(type(a))
+a = "a"
+print(type(a))
--- basic/range_test01.py
+++ basic/range_test01.py
@@ -1,4 +1,6 @@
-# 打印9 * 9乘法表
+"""
+打印9 * 9乘法表
+"""
 for x in range(1, 10):
     for y in range(1, x + 1):
         print("{0} * {1} = {2}".format(x, y, x * y), end="\t")
