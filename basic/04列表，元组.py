5f56e807d247d5144030e5debdcc871b52b6394a
li.chunlong <lcl253137@alibaba-inc.com>
2020-10-13 16:27:18 +0800

    范例添加

--- basic/04列表，元组.py
+++ basic/04列表，元组.py
@@ -43,8 +43,8 @@ print(c)
 c.reverse()
 print(c)
 
-for i, chrs in enumerate(c):
-    print("下标：%d----值：%s" % (i, chrs))
+for item, chrs in enumerate(c):
+    print("下标：%d----值：%s" % (item, chrs))
 
 c[2] = "abcdefg"
 print(c)
@@ -76,23 +76,74 @@ print(person)
 print(person["aaa"])
 abc = {"a", "b", "c"}
 print(abc)
+
+# 通过dict()函数创建字典
+person = dict(name="melo", age=23, gender="男")
+print(person)
+
+# 通过双值子序列创建字典
+person = dict([("name", "melo"), ("age", 23), ("gender", "男")])
+print(person)
+print(len(person))
+print("age" in person)
+
+# key不存在则报错KeyError
+# print(person["undefined"])
+
+# key不存在则返回None，也可指定默认返回参数
+print(person.get("undefined"))
+print(person.get("undefined", "defaultParam"))
+
 person["aaa"] = 100
 print(person)
 person["ddd"] = 200
 print(person)
-del (person["bbb"])
-# del(person)
+# 向字典中添加Key、value，存在不做任何操作，返回原值；不存在则添加该key
+person.setdefault("ddd", 300)
 print(person)
-person.clear()
+person.setdefault("eee", 400)
 print(person)
 
+# update()函数，将其他字典中的值添加到当前字典中,key重复则用后者value覆盖当前
+teacher = {"a": 1, "b": 2}
+student = {"x": 3, "a": 4}
+teacher.update(student)
+print(teacher)
+
+del person["ddd"]
+# del person
+print(person)
+
+# popitem()随机删除一个字典值，一般删除最后一个，返回删除的key-value元组
+deleteItem = person.popitem()
+print(person)
+print("deleteItem=", deleteItem)
+
+# pop()删除指定字典值
+person.pop("aaa")
+person.pop("undefined", "defaultParam")
+
+# person.clear()
+# print(person)
+
+# copy()对字典进行浅复制，复制以后的对象和原对象之间独立
+source = {"a", "b", "c"}
+dest = source.copy()
+print("source=", source, id(source))
+print("dest=", dest, id(dest))
+
+# 1、遍历key
 for key in person.keys():
-    print(key)
+    print(key, person[key])
+# 2、遍历value
 for value in person.values():
     print(value)
-for i in person.items():
-    print(i)
+# 3、遍历对象
+for item in person.items():
+    print(item)
+# 4、遍历key, value
 for key, value in person.items():
     print(key, value)
+
 a = set([1, 2, 3, 4])
 print(a)
