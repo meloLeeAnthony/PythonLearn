3829700d9c25aaff704cfab1832ba8a990cf6ca1
li.chunlong <lcl253137@alibaba-inc.com>
2020-11-12 18:00:02 +0800

    函数-4:返回值 & docString

--- basic/05函数.py
+++ basic/05函数.py
@@ -7,7 +7,7 @@
 
 def test():
     """
-        简单打印
+    简单打印
     """
     print("python真简单")
 
@@ -17,7 +17,7 @@ test()
 
 def add(paramA, paramB):
     """
-        加法方法
+    加法方法
     :param paramA: 参数a
     :param paramB: 参数b
     """
@@ -27,9 +27,22 @@ def add(paramA, paramB):
 add(1, 2)
 
 
+def add(paramA: int):
+    """
+    加法方法
+    :param paramA: 参数a
+    """
+    print(paramA)
+
+
+add(1)
+
+help(add)
+
+
 def add2(paramA, paramB=2):
     """
-        加法方法
+    加法方法
     :param paramA: 参数a
     :param paramB: 参数b，默认值为2
     """
@@ -41,7 +54,7 @@ add2(1)
 
 def function(paramA, paramB, *args, **kwargs):
     """
-        可变参数的函数
+    可变参数的函数
     :param paramA: 参数a
     :param paramB: 参数b
     :param args: 将多个参数收集到一个“元组”对象中，存放所有未命名的变量参数
@@ -59,7 +72,7 @@ function(1, 2, 3, 4, 5, 6, 7, 8, m=7, x=9, y=10)
 
 def add3(objectParam):
     """
-        测试形参是对象的方法
+    测试形参是对象的方法
     :param objectParam: 对象形参
     """
     objectParam += objectParam
@@ -72,23 +85,9 @@ add3(a)
 print(a)
 
 
-def func(paramA, paramB):
-    """
-        测试函数作为入参
-    :param paramA: 参数a
-    :param paramB: 参数b
-    :return:
-    """
-    returnC = paramA + paramB
-    return returnC
-
-
-print(func(2, 3))
-
-
 def funUnpack(paramA, paramB, paramC):
     """
-        参数的解包/拆包
+    参数的解包/拆包
     :param paramA: 参数a
     :param paramB: 参数b
     :param paramC: 参数c
@@ -104,10 +103,29 @@ d = {'paramA': 10, 'paramB': 20, 'paramC': 30}
 funUnpack(**d)
 
 
-def func2(a):
-    b = 100 + a
-    c = 200 + a
-    return b, c
+def func(paramA, paramB):
+    """
+    测试函数返回值
+    :param paramA: 参数a
+    :param paramB: 参数b
+    :return:
+    """
+    returnC = paramA + paramB
+    return returnC
+
+
+print(func(2, 3))
+
+
+def func2(paramA):
+    """
+    测试多个函数返回值
+    :param paramA: 参数A
+    :return: 多个返回值
+    """
+    returnB = 100 + paramA
+    returnC = 200 + paramA
+    return returnB, returnC
 
 
 b, c = func2(200)
