e04c193fd70e7ed9c2f6137b66f0e158a3261105
li.chunlong <lcl253137@alibaba-inc.com>
2021-05-08 16:09:35 +0800

    doc注释添加

--- 05-object面向对象/07myOop_01.py
+++ 05-object面向对象/07myOop_01.py
@@ -1,16 +1,21 @@
 class Student:
-
+    """
+    Student类对象
+    """
     def __init__(self, name, age):  # 构造方法第一个参数必须为self
         self.name = name  # 实例属性
         self.age = age
 
     def say_score(self):  # self必须位于第一个参数
+        """
+        类的实例属性打印
+        """
         print("{0}的年龄是{1}".format(self.name, self.age))
 
 
 s1 = Student("Melo", 30)  # s1是实例对象，自动调用__init__()方法
 s1.say_score()
 
-s1.salary = 310000
+s1.salary = 10000
 
 print(s1.salary)
