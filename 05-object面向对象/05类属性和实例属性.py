6ea6ff4344660455faf5eb383f1250d929c0fdaa
li.chunlong <lcl253137@alibaba-inc.com>
2021-05-08 15:59:25 +0800

    类的属性与方法区分

--- 05-object面向对象/05类属性和实例属性.py
+++ 05-object面向对象/05类属性和实例属性.py
@@ -6,6 +6,10 @@ Created on 2018年10月10日
 
 
 class Person(object):
+    """
+    Person对象
+    """
+    # 类属性: 直接在类中定义的属性; 可以通过类或类的实例访问
     name = "zhangsan"
 
     #     def __init__(self, name,age):
@@ -13,19 +17,29 @@ class Person(object):
     #         self.age=age
     @classmethod
     def test(cls):
+        """
+        类方法: 第一个参数为cls，代表当前的类对象; 可以通过类或类的实例调用
+        """
         print("类方法")
 
     def test2(self):
+        """
+        实例方法，第一个参数为self; 可以通过类或类的实例调用
+        """
         print("test2")
 
     @staticmethod
     def test3():
+        """
+        静态方法: 只能通过类对象调用
+        """
         print("test3")
 
 
 # p = Person("lisi", 12)
 p = Person()
 print(p.name)
+# 实例属性: 通过实例对象添加的属性，类对象无法访问
 p.name = "wangwu"
 print(p.name)
 print(Person.name)
