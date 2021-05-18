dcb2233032078542fe050e057574e3e12f978369
li.chunlong <lcl253137@alibaba-inc.com>
2021-05-18 11:47:46 +0800

    IO操作-3:seek & tell

--- 06-io文件处理/seek指针移动.py
+++ 06-io文件处理/seek指针移动.py
@@ -7,4 +7,5 @@ with open("o.txt", encoding="utf-8") as f:
     print("读取的内容：{0}".format(str(f.readline().strip('\n'))))
     print(f.tell())
     f.seek(0)
-    print("读取的内容：{0}".format(str(f.readline())))
+    print("读取的内容：{0}".format(str(f.readline().strip('\n'))))
+    print(f.tell())
